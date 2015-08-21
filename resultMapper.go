13f00d39b79e4a6aa776977d084cddaa1919d64f
chris-pardy <chris@Computer-from-the-Black-Lagoon.local>
2015-08-20 22:54:07 -0400

    updated resultMapper and resultMapper_test to be less perscriptive

--- resultMapper.go
+++ resultMapper.go
@@ -1,70 +1,104 @@
-package godapper
+package dapper
 
 import (
 	"database/sql"
 	"reflect"
-	"fmt"
 	"errors"
+	"fmt"
 )
 
+var (
+	EOR = errors.New("End of Rows")
+)
+
+type Rows interface {
+	Err() error
+	Columns() ([]string, error)
+	Next() bool
+	Scan(dest ...interface{}) error
+}
+
 type valueScanner struct {
-	value reflect.Value
-	index []int
+	field reflect.Value
 }
 
 func (v *valueScanner) Scan(src interface{}) error {
-	field := v.value.FieldByIndex(v.index)
-	if !field.CanSet() {
-		return errors.New("")
+	if !v.field.CanSet() {
+		return fmt.Errorf("Unable to set field %v", v.field)
 	}
-	field.Set(reflect.ValueOf(src))
+	v.field.Set(reflect.ValueOf(src))
 	return nil
 }
 
-func getScanner(t reflect.Type, columnName string) (func(reflect.Value)sql.Scanner, error) {
-	field,found := t.FieldByName(columnName)
-	if !found {
-		return nil, fmt.Errorf("No field named %v in type %v",columnName,t.Name())
-	}
-	index := field.Index
-	return func(v reflect.Value) sql.Scanner {
-		return &valueScanner{v, index}
-	}, nil
+type mapScanner struct {
+	key string
+	m map[string]interface{}
 }
 
-func mapScanners (scanners []func(reflect.Value)sql.Scanner, rowValue reflect.Value) []interface{} {
-	rSlice := make([]interface{},len(scanners))
-	for i,scanner := range(scanners) {
-		rSlice[i] = scanner(rowValue)
-	}
-	return rSlice
+func (m *mapScanner) Scan(src interface{}) error {
+	m.m[m.key] = src
+	return nil
 }
 
-func mapResult(r rows, returnType reflect.Type,
-		cache func(reflect.Type,string)(func(reflect.Value)sql.Scanner, error)) ([]interface{}, error) {
-	cols, err := r.Columns()
-	if err != nil {
-		return nil, err
-	}
-	scanners := make([]func(reflect.Value)sql.Scanner,len(cols))
-	for i,col := range(cols){
-		s, err := cache(returnType, col)
-		scanners[i] = s
-		if err != nil {
-			return nil, err
-		}
-	}
-	rowValues := make([]interface{},0,1)
-	for r.Next() {
-		rowValue := reflect.New(returnType)
-		err := r.Scan(mapScanners(scanners, reflect.Indirect(rowValue))...)
-		if err != nil {
-			return nil, err
-		}
-		rowValues = append(rowValues,rowValue.Interface())
-	}
-	if err := r.Err(); err != nil {
-		return nil, err
-	}
-	return rowValues, nil
+type arrayScanner struct {
+	index int
+	array []interface{}
 }
+
+func (a *arrayScanner) Scan(src interface{}) error {
+	if len(a.array) < a.index {
+		return fmt.Errorf("Index %v is out of slice range", a.index)
+	}
+	a.array[a.index] = src
+	return nil
+}
+
+type Result struct {
+	Rows
+}
+
+// Lets you read the results of a SQL query as though they were and encoded stream
+// Decode behaves differently depending on the type of d
+//    If d is a map[string]interface{} the values are populated with keys set to the column names
+//    If d is an []interface{} the position of the column number in the Result is used
+//    If d is a struct or a pointer to a struct then the field names are used
+func (r *Result) Decode(d interface{}) error {
+	if r == nil {
+		return errors.New("Cannot decode on nil result")
+	}
+	hasNext := r.Next()
+	if !hasNext {
+		return EOR
+	}
+	cols,_ := r.Columns()
+	scanners := make([]interface{},len(cols))
+	var findScanner func(interface{},int,string) sql.Scanner
+	switch d.(type) {
+	case map[string]interface{}:
+		findScanner = func(d interface{}, i int, columnName string) sql.Scanner {
+			return &mapScanner{columnName, d.(map[string]interface{})}
+		}
+	case []interface{}:
+		findScanner = func (d interface{}, i int, columnName string) sql.Scanner {
+			return &arrayScanner{i,d.([]interface{})}
+		}
+	default:
+		findScanner = func(d interface{}, i int, columnName string) sql.Scanner {
+			v := reflect.ValueOf(d)
+			field := reflect.Indirect(v).FieldByName(columnName)
+			return &valueScanner{field}
+		}
+	}
+	for i,col := range(cols) {
+		scanners[i] = findScanner(d, i, col)
+	}
+	err := r.Scan(scanners...)
+	return err
+}
+
+func MapResult(rows Rows) Result {
+	return Result{rows}
+}
+
+
+
--- resultMapper_test.go
+++ resultMapper_test.go
@@ -1,9 +1,47 @@
-package godapper
+package dapper
 
 import (
-	"reflect"
+	"database/sql"
 	"testing"
 )
+
+type mockRows struct {
+	err func() error
+	cols []string
+	rows [][]interface{}
+	index int
+}
+
+func (m mockRows) Err() error {
+	return m.err()
+}
+
+func (m mockRows) Columns() ([]string, error) {
+	return m.cols, nil
+}
+
+func (m *mockRows) Next() bool {
+	m.index++;
+	if m.index < len(m.rows) {
+		return true;
+	}
+	return false
+}
+
+func (m *mockRows) Scan(dest ...interface{}) error {
+	for i,col := range(m.rows[m.index]) {
+		scanner, ok := dest[i].(sql.Scanner)
+		if ok {
+			err := scanner.Scan(col)
+			if err != nil {
+				return err
+			}
+		} else {
+			dest[i] = col
+		}
+	}
+	return nil
+}
 
 type test1 struct {
 	A, B int
@@ -12,36 +50,132 @@ type test1 struct {
 func TestMapResultReturnsListOfStructs(t *testing.T) {
 	mr := mockRows{func()error{
 			return nil
-		}, -1,[]string{"A","B"},[][]interface{}{[]interface{}{1,2}}}
-	results,_ := mapResult(&mr,reflect.TypeOf(test1{}),getScanner)
-	if len(results) != 1 {
-		t.Fatalf("Expected 1 result but had %v", len(results))
+		}, []string{"A","B"},[][]interface{}{[]interface{}{1,2}}, -1}
+	result := MapResult(&mr)
+	dest := test1{}
+	pos := 0
+	for {
+		err := result.Decode(&dest)
+		if err != nil {
+			break
+		}
+		if dest.A != 1 {
+			t.Fatalf("Expected A value of 1 but was %v",dest.A)
+		}
+		if dest.B != 2 {
+			t.Fatalf("Expected B value of 2 but was %v",dest.B)
+		}
+		dest = test1{}
+		pos++
 	}
-	r := results[0]
-	v, ok := r.(*test1)
-	if !ok {
-		t.Fatalf("Expected *test1 type but was %v",reflect.TypeOf(r))
+	if pos != 1 {
+		t.Fatalf("Expected 2 rows but was %v",pos)
 	}
-	if v.A != 1 {
-		t.Fatalf("Expected A value of 1 but was %v", v.A)
+}
+
+func TestMapResultWorksWithMaps(t *testing.T) {
+	mr := mockRows{func()error {
+		return nil
+		}, []string{"A","B"},
+		[][]interface{}{[]interface{}{"U","V"}},-1}
+	result := MapResult(&mr)
+	dest := make(map[string]interface{})
+	len := 0
+	for {
+		err := result.Decode(dest)
+		if err != nil {
+			break
+		}
+		if dest["A"] != "U" {
+			t.Fatalf("Expected A value of U but was %v",dest["A"])
+		}
+		if dest["B"] != "V" {
+			t.Fatalf("Expected B value of V but was %v",dest["B"])
+		}
+		dest = make(map[string]interface{})
+		len++
 	}
-	if v.B != 2 {
-		t.Fatalf("Excepted B value of 2 but was %v", v.B)
+	if len != 1 {
+		t.Fatalf("Expected length of 1 but was %v",len)
+	}
+}
+
+func TestMapResultWorksWithSlices(t *testing.T) {
+	mr := mockRows{func()error{return nil},
+			[]string{"A","B"},
+			[][]interface{}{[]interface{}{1,"U"}}, -1}
+	result := MapResult(&mr)
+	dest := make([]interface{},2)
+	len := 0
+	for {
+		err := result.Decode(dest)
+		if err != nil {
+			break
+		}
+		if dest[0] != 1 {
+			t.Fatalf("Expected first column value of 1 but was %v", dest[0])
+		}
+		if dest[1] != "U" {
+			t.Fatalf("Expected second column value of U but was %v", dest[1])
+		}
+		dest = make([]interface{},2)
+		len++
+	}
+	if len != 1 {
+		t.Fatalf("Expected length of 1 but was %v", len)
 	}
 }
 
 func TestMapResultReturnsListOfStructsPerRow(t *testing.T) {
 	mr := mockRows{func()error {
 			return nil
-		}, -1, []string{"A","B"},
+		}, []string{"A","B"},
 		[][]interface{}{
 			[]interface{}{1,2},
 			[]interface{}{3,4},
 			[]interface{}{5,6},
-			[]interface{}{7,8}}}
-	results,_ := mapResult(&mr, reflect.TypeOf(test1{}),getScanner)
-	if len(results) != 4 {
-		t.Fatalf("Expected 4 results but had %v", len(results))
+			[]interface{}{7,8}}, -1}
+	result := MapResult(&mr)
+	dest := &test1{}
+	pos := 0
+	for {
+		err := result.Decode(dest)
+		if err != nil {
+			break
+		}
+		if pos == 0 {
+			if dest.A != 1 {
+				t.Fatalf("Expected A value of 1 but was %v",dest.A)
+			}
+			if dest.B != 2 {
+				t.Fatalf("Expected B value of 2 but was %v", dest.B)
+			}
+		} else if pos == 1 {
+			if dest.A != 3{
+				t.Fatalf("Expected A value of 3 but was %v", dest.A)
+			}
+			if dest.B != 4 {
+				t.Fatalf("Expected B value of 4 but was %v", dest.B)
+			}
+		} else if pos == 2 {
+			if dest.A != 5 {
+				t.Fatalf("Expected A value of 5 but was %v",dest.A)
+			}
+			if dest.B != 6 {
+				t.Fatalf("Expected B value of 6 but was %v", dest.B)
+			}
+		} else if pos == 3 {
+			if dest.A != 7 {
+				t.Fatalf("Expected A value of 7 but was %v",dest.A)
+			}
+			if dest.B != 8 {
+				t.Fatalf("Expected B value of 8 but was %v",dest.B)
+			}
+		}
+		dest = &test1{}
+		pos++
 	}
-
+	if pos != 4 {
+		t.Fatalf("Expected length of 4 but was %v",pos)
+	}
 }
